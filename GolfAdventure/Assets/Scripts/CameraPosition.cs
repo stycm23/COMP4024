95e426dad5d49e24082c362a93f7a4ada1badeec
Daniel Long <psydl2@examil.nottingham.ac.uk>
2023-02-22 17:36:23 +0000

    Added camera tracking based on ball velocity. TODO - Add initialisation conditions

--- GolfAdventure/Assets/Scripts/CameraPosition.cs
+++ GolfAdventure/Assets/Scripts/CameraPosition.cs
@@ -1,17 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 public class CameraPosition : MonoBehaviour
 {
     public GameObject ball;
+    public Rigidbody ballRB;
     public int cameraAngle = 20;
+
+    void Start()
+    {
+
+    }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = new Vector3(ball.transform.position.x, ball.transform.position.y, ball.transform.position.z);
+        Vector3 velocity = ballRB.velocity;
+        float angle = getAngleFromVelocity(velocity);
 
-        transform.rotation = Quaternion.Euler(cameraAngle, ball.transform.rotation.eulerAngles.x, 0);
+        transform.position = new Vector3(ball.transform.position.x, ball.transform.position.y, ball.transform.position.z);
+        
+        transform.rotation = Quaternion.Euler(cameraAngle, angle, 0);
+    }
+
+    float getAngleFromVelocity(Vector3 velocity)
+    {
+        double angleRad = Math.Atan2(velocity.x, velocity.z);
+        double angleDeg = angleRad * (180 / Math.PI);
+
+        print(angleDeg);
+        return (float)angleDeg;
     }
 }
