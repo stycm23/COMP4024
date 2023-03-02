73a23d195a131f05e63df7d4307249406564dc58
Daniel Long <psydl2@examil.nottingham.ac.uk>
2023-03-02 11:30:30 +0000

    Reverted to camera following ball position not velocity

--- GolfAdventure/Assets/Scripts/CameraPosition.cs
+++ GolfAdventure/Assets/Scripts/CameraPosition.cs
@@ -19,29 +19,16 @@ public class CameraPosition : MonoBehaviour
     void FixedUpdate()
     {
         Vector3 velocity = ballRB.velocity;
-        float angle = getAngleFromVelocity(velocity);
+        //float angle = getAngleFromVelocity(velocity); //Depreciated
 
         transform.position = ball.transform.position;
-        //  transform.position = new Vector3(ball.transform.position.x, ball.transform.position.y, ball.transform.position.z);
-        /*float change = 0;
-        if ((transform.rotation.eulerAngles.y > angle + 0.5) | (transform.rotation.eulerAngles.y < angle - 0.5))
-        {
-            print("Rotating");
-            float diff = angle - transform.rotation.eulerAngles.y;
-            change = diff/smoothingRate;
-
-            if (change > 0) {change = 0-change;}
-        }
-        print(change);
-        transform.Rotate(0.0f, change, 0.0f, Space.World);*/
-        //transform.rotation = Quaternion.Euler(cameraAngle, angle, 0);
     }
 
-    float getAngleFromVelocity(Vector3 velocity)
+    float getAngleFromVelocity(Vector3 velocity) // Depreciated
     {
         double angleRad = Math.Atan2(velocity.x, velocity.z);
         double angleDeg = angleRad * (180 / Math.PI);
 
-        return (float)angleDeg;
+        return (float)angleDeg; 
     }
 }
