71ff5981a3671b5930c85637a8fb1e7ecce95b00
Daniel Long <psydl2@examil.nottingham.ac.uk>
2023-03-03 23:56:42 +0000

    Added untracked file

---
+++ GolfAdventure/Assets/Scripts/CameraControl.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+
+    public float lookSens = 10;
+    public GameObject goal;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.R) && !Input.GetKey(KeyCode.Mouse0))
+        {
+            Vector3 planarGoal = goal.transform.position;
+            planarGoal.y = transform.position.y;
+            transform.LookAt(planarGoal);
+        }
+
+        if (Input.GetKey(KeyCode.Mouse1) && !Input.GetKey(KeyCode.Mouse0)) 
+        {
+            print("RMB Pressed");
+            float rot = Input.GetAxis("Mouse X");
+            transform.Rotate(0.0f, 10 * Input.GetAxis("Mouse X"), 0.0f, Space.Self);
+        }
+    }
+}
